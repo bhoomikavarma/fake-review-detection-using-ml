c2f35cc0b26bac866d5d70e09f9dfe8edbcf93b6
bhoomikavarma <bhoomikapotharaju45@gmail.com>
2025-07-20 14:28:17 +0530

    Add text processing module

---
+++ utils/preprocessing.py
@@ -0,0 +1,15 @@
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+stemmer = PorterStemmer()
+
+def preprocess_text(text):
+    text = str(text).lower()
+    text = ''.join([c for c in text if c not in string.punctuation])
+    words = text.split()
+    words = [stemmer.stem(word) for word in words if word not in stop_words]
+    return ' '.join(words)
